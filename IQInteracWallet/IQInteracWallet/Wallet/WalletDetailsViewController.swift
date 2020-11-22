0312bc1a8e4ffb2aab30b4a09603f076f21c0927
Phanindra <>
2020-11-21 19:26:02 -0800

    minor update

--- IQInteracWallet/IQInteracWallet/Wallet/WalletDetailsViewController.swift
+++ IQInteracWallet/IQInteracWallet/Wallet/WalletDetailsViewController.swift
@@ -28,22 +28,16 @@ class WalletDetailsViewController: UIViewController {
         emptyInfoImageView.addSubview(WalletInfoCellView!)
         WalletInfoCellView?.frame = CGRect(x: -110, y: -50, width: 300, height: 150)
         WalletInfoCellView?.configureView(for: walletType.rawValue)
-        var imgName = ""
         switch walletType {
         case .totalBalance:
             emptyInfoLabel.text = "You haven't made any transactions yet."
-            imgName = "ic_totalbalance"
         case .amountLoaded:
-            imgName = "ic_amountloaded"
             emptyInfoLabel.text = "You haven't made any transactions yet. Start with adding money to your secured wallet and use money from wallet to pay"
         case .amountReceived:
-            imgName = "ic_amountreceived"
             emptyInfoLabel.text = "You haven't received any money from your family/friends yet. Dont worry we will alert if you receive money"
         case .amountSent:
-            imgName = "ic_amountsent"
             emptyInfoLabel.text = "You haven't made any transactions yet. Start sending money now."
         case .amountUsed:
-            imgName = "ic_amountused"
             emptyInfoLabel.text = "You haven't made any transactions yet. Start making an order"
         }
         emptyInfoLabel.font = UIFont.init(name: "Helvetica-Regular", size: 15)
