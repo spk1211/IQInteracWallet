1b3990e9a503a1718401a0f00c9a0595f973e50b
Phanindra <>
2020-11-21 18:44:46 -0800

    Initial commit

---
+++ IQInteracWallet/IQInteracWallet/IQInteracWallet.h
@@ -0,0 +1,19 @@
+//
+//  IQInteracWallet.h
+//  IQInteracWallet
+//
+//  Created by Phanindra on 21/11/20.
+//  Copyright © 2020 Jettysoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IQInteracWallet.
+FOUNDATION_EXPORT double IQInteracWalletVersionNumber;
+
+//! Project version string for IQInteracWallet.
+FOUNDATION_EXPORT const unsigned char IQInteracWalletVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IQInteracWallet/PublicHeader.h>
+
+
---
+++ IQInteracWallet/IQInteracWallet/IQWallet.swift
@@ -0,0 +1,19 @@
+//
+//  IQWallet.swift
+//  IQInteracWallet
+//
+//  Created by Phanindra on 21/11/20.
+//  Copyright © 2020 Jettysoft. All rights reserved.
+//
+
+import Foundation
+
+public class IQWallet {
+    
+    private init() {}
+    
+    public static func AddWallet(){
+        debugPrint("Wallet Added")
+    }
+    
+}
